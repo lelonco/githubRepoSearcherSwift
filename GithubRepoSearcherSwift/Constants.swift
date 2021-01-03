d4b0d5d3a1ab5cc64850beec8d274e1f0734e343
YaroslavArk <fast.yarik@gmail.com>
2021-01-03 20:06:10 +0200

    Added new file

---
+++ GithubRepoSearcherSwift/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TestApi
+//
+//  Created by Yaroslav on 15.12.2020.
+//
+
+import Foundation
+
+struct Constants {
+    static let internetReacibilityCahnged = "internetReacibilityCahngedNotification"
+    static let noInternetErorrs: [URLError.Code] = [.timedOut,.networkConnectionLost,.notConnectedToInternet]
+}
