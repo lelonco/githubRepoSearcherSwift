e9eb985976a54b1d988a61b49b716ca338fdf108
YaroslavArk <fast.yarik@gmail.com>
2021-01-09 05:01:08 +0200

    Added new search network request

--- GithubRepoSearcherSwift/Network/RequestBuilder.swift
+++ GithubRepoSearcherSwift/Network/RequestBuilder.swift
@@ -14,6 +14,13 @@ enum SortingType:String {
 
 class RequestBuilder {
     
+    static func searchRepo(text: String) -> GitApiRequest {
+        let gitApi = GitApiRequest(endPoint: "search/repositories")
+        let perPage = 50
+        let queryparam = ["q":text,"sort":"stars", "order": "asc", "per_page": "\(perPage)"]
+        gitApi.queryParam = queryparam
+        return gitApi
+    }
 //    private static let encoder = JSONEncoder()
 //    private static let accessManager = AccessManager.shared
 //    
