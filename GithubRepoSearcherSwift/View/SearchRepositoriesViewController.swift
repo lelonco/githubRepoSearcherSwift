e78c6ac9c0699d690fe61754471b76f28242c826
YaroslavArk <fast.yarik@gmail.com>
2021-01-09 05:02:56 +0200

    Refactored search repository vc

--- GithubRepoSearcherSwift/View/SearchRepositoriesViewController.swift
+++ GithubRepoSearcherSwift/View/SearchRepositoriesViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreData
 
 class SearchRepositoriesViewController: UITableViewController {
-    var searchVM: SearchResult? {
+    var searchVM: SearchReposirotiesViewModel? {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -22,7 +22,7 @@ class SearchRepositoriesViewController: UITableViewController {
 
     init() {
         super.init(nibName: nil, bundle: nil)
-
+        searchVM = SearchReposirotiesViewModel()
     }
     
     required init?(coder: NSCoder) {
@@ -33,57 +33,43 @@ class SearchRepositoriesViewController: UITableViewController {
         self.navigationItem.titleView = searchBar
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(makeRequest))
 
-//        makeRequest()
-//        try! fetchedResultController.performFetch()
-        
         super.viewDidLoad()
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Celll")
+        self.tableView.register(RepostiryTableViewCell.self, forCellReuseIdentifier: "Celll")
         self.tableView.separatorStyle = .none
         view.backgroundColor = .green
+        searchVM?.reloadUI = {
+            self.tableView.reloadData()
+        }
         // Do any additional setup after loading the view.
     }
+    
     @objc
     func makeRequest() {
-        
+        guard let text = searchBar.text else {
+            return
+        }
+        searchVM?.fetchRepos(searchText: text)
 //        findFakeRepos()
 //        findRepos()
         
     }
-//    func findFakeRepos() {
-//        let path = Bundle.main.path(forResource: "JSON", ofType: "json")
-//        let object = try! Data(contentsOf: URL(fileURLWithPath: path!) , options: .mappedIfSafe)
-//        let decoder = JSONDecoder()
-//        decoder.userInfo[CodingUserInfoKey.context!] = self.persistantContainer.viewContext
-//        let items = (try! JSONSerialization.jsonObject(with: object as! Data, options: []) as? [String:Any])?["items"]
-//        let data = try? JSONSerialization.data(withJSONObject: items, options: [.prettyPrinted])
-//
-//        let repo = try! decoder.decode([Repository].self, from: data!)
-//        let searchResult = SearchResult(context: self.persistantContainer.viewContext)
-//        searchResult.searchRequest = "albion"
-//        searchResult.addToResults(NSSet(array: repo))
-//        print(repo)
-//        print(try! JSONSerialization.jsonObject(with: object as! Data, options: []))
-//        self.saveContext()
-//        try! fetchedResultController.performFetch()
-//        print(fetchedResultController.sections)
-//    }
+
 
     
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        searchVM?.numberOfSections() ?? 1
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        searchVM?.results?.count ?? 0
+        searchVM?.numberOfRowsInSection() ?? 0
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "Celll")
-//        cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Celll")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celll") as? RepostiryTableViewCell
         
-        let repos = (searchVM?.results?.allObjects as? Array<Repository>)
+        let vm = searchVM?.cellVM(for: indexPath)
         
-        cell!.textLabel?.text = repos?[indexPath.row].fullName
-        cell!.detailTextLabel?.text = repos?[indexPath.row].language
+        cell!.textLabel?.text = vm?.fullName
+        cell!.detailTextLabel?.text = vm?.language
         cell!.imageView?.image = UIImage(systemName: "repeat.circle.fill")
         cell?.accessoryView = UIImageView(image: UIImage(systemName: "paperplane.circle.fill"))
         return cell!
