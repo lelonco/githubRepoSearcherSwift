93cb3537d57db4a2a1beb8f2075d9fcc2a7a4991
YaroslavArk <fast.yarik@gmail.com>
2021-01-14 01:12:54 +0200

    Added error for decode func

--- GithubRepoSearcherSwift/Models/Repository/Repository+CoreDataClass.swift
+++ GithubRepoSearcherSwift/Models/Repository/Repository+CoreDataClass.swift
@@ -25,7 +25,8 @@ public class Repository: NSManagedObject, Decodable {
               let managedObjectContext = decoder.userInfo[contextUserInfoKey] as? NSManagedObjectContext,
               let entity = NSEntityDescription.entity(forEntityName: "Repository", in: managedObjectContext)
         else {
-            fatalError("decode failure")
+            let error = NSError(domain: "Decode failure", code: -999, userInfo: nil)
+            throw(error)
         }
         // Super init of the NSManagedObject
         self.init(entity: entity, insertInto: managedObjectContext)
