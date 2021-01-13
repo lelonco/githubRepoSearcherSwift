c563bcb3ca8c81a03899095b89b7ace72872f8bc
YaroslavArk <fast.yarik@gmail.com>
2021-01-14 01:16:32 +0200

    Added cached results

--- GithubRepoSearcherSwift/View/CachedResults/CachedResultsViewController.swift
+++ GithubRepoSearcherSwift/View/CachedResults/CachedResultsViewController.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import UIKit
+
+class CachedResultsViewController: UITableViewController {
+    
+    var cachedResultsViewModel: CachedResultsViewModel?
+ 
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        cachedResultsViewModel = CachedResultsViewModel()
+        cachedResultsViewModel?.reloadUI = {
+            self.tableView.reloadData()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        self.title = "Cached results"
+        tableView.register(RepostiryTableViewCell.self, forCellReuseIdentifier: "Celll")
+    }
+    //MARK: - UITableViewDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        cachedResultsViewModel?.numberOfSections() ?? 0
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cachedResultsViewModel?.numberOfRowsInSection() ?? 0
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celll") as? RepostiryTableViewCell
+        
+        let vm = cachedResultsViewModel?.cellVM(for: indexPath)
+        
+        cell!.textLabel?.text = vm?.titleText
+        cell!.detailTextLabel?.text = vm?.subtitleText
+        cell!.imageView?.image = UIImage(systemName: "repeat.circle.fill")
+        cell?.accessoryView = UIImageView(image: UIImage(systemName: "paperplane.circle.fill"))
+        return cell!
+    }
+    
+    //MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+    
+}
--- GithubRepoSearcherSwift/ViewModels/CachedResultsViewModel.swift
+++ GithubRepoSearcherSwift/ViewModels/CachedResultsViewModel.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import CoreData
+class CachedResultsViewModel: NSObject {
+    
+    private var searchResults: [SearchResult] = []
+    
+    lazy var fetchedResultController: NSFetchedResultsController<SearchResult> = {
+        let request: NSFetchRequest<SearchResult> = SearchResult.fetchRequest()
+        let sort = NSSortDescriptor(key: #keyPath(SearchResult.searchRequest), ascending: false)
+        request.sortDescriptors = [sort]
+        let controller = NSFetchedResultsController(fetchRequest: request,
+                                                    managedObjectContext: DatabaseManager.shared.persistantContainer.newBackgroundContext(),
+                                                    sectionNameKeyPath: nil,
+                                                    cacheName: nil)
+        return controller
+    }()
+    
+    var reloadUI: (() -> ())? = nil
+
+    override init() {
+        super.init()
+        fetchedResultController.delegate = self
+        try! fetchedResultController.performFetch()
+        searchResults.append(contentsOf: fetchedResultController.fetchedObjects ?? []) 
+    }
+    
+    func cellVM(for indexPath: IndexPath) -> CellViewModel {
+        let result = searchResults[indexPath.row]
+        let name = result.searchRequest ?? "Can't find text of search request "
+        let resultsText = "\((result.results?.count ?? 0)) was found"
+        return CellViewModel(titleText:name, subtitleText:resultsText)
+    }
+
+    func numberOfRowsInSection() -> Int {
+        searchResults.count
+    }
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func titleForHeaderInSection(section: Int) -> String {
+        return ""
+    }
+}
+
+extension CachedResultsViewModel: NSFetchedResultsControllerDelegate {
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith diff: CollectionDifference<NSManagedObjectID>) {
+        self.reloadUI?()
+    }
+}
