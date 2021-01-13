98b7e1a19f5ba8571fac7b26ed214403f654b2b0
YaroslavArk <fast.yarik@gmail.com>
2021-01-14 01:10:37 +0200

    Added persistentContainer subclass

--- GithubRepoSearcherSwift/CoreData/PersistentContainer.swift
+++ GithubRepoSearcherSwift/CoreData/PersistentContainer.swift
@@ -5,4 +5,17 @@
 //  Created by Yaroslav on 11.01.2021.
 //
 
-import Foundation
+import CoreData
+
+class PersistentContainer: NSPersistentContainer {
+
+    func saveContext(backgroundContext: NSManagedObjectContext? = nil) {
+        let context = backgroundContext ?? viewContext
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
+    }
+}
