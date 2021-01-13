26ba714e1fd2033394273a72f8a710206f34c7af
YaroslavArk <fast.yarik@gmail.com>
2021-01-14 01:09:35 +0200

    Added databaseManager

--- GithubRepoSearcherSwift/CoreData/DatabaseManager.swift
+++ GithubRepoSearcherSwift/CoreData/DatabaseManager.swift
@@ -5,4 +5,41 @@
 //  Created by Yaroslav on 11.01.2021.
 //
 
-import Foundation
+import CoreData
+
+class DatabaseManager {
+    let modelName: String
+    static let shared = DatabaseManager(modelName: "Model")
+    
+    lazy var persistantContainer: PersistentContainer = {
+        let persistantContainer = PersistentContainer(name: modelName)
+
+        persistantContainer.loadPersistentStores { (storeDescription, error) in
+            self.persistantContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+            if let error = error {
+                print("Unresolved error: \(error.localizedDescription)")
+            }
+        }
+        return persistantContainer
+    }()
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        persistantContainer.viewContext
+    }()
+    func saveContext(_ managedContext: NSManagedObjectContext? = nil) {
+        let context = managedContext ?? self.managedContext
+        if context.hasChanges {
+            do {
+                try persistantContainer.viewContext.save()
+            } catch {
+                print("An error occurred while saving: \(error)")
+            }
+        }
+    }
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    
+}
